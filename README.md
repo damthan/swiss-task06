f52e9e6bde7691932e7c6ebcdbe08807cb8f8256
dam than <thandam2009@gmail.com>
2024-09-07 17:29:21 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+change to root folder and install:
+```shell
+npm install --force
+```
+Deploy proxy
+
+```shell
+npm run deploy
+```
+
+or
+```shell
+npx hardhat run scripts/deploy.ts --network swisstronik
+```
---
+++ scripts/utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0xa266fd30E2BA79228B22Fc7D27b4e07ABEf5913e'
+export const ProxyAdmin = '0xf00da9EF145218e7cb0AD1e5E919591b2BF0dC0b'
+export const SWTRImplementation = '0xd4964394A85c88DBfbd54042d62391df33D44D63'
+    
